b6c3ce961f009cc3d49e8de6a833da600a316ffa
VasiliyShannikov <95113531+VasiliyShannikov@users.noreply.github.com>
2022-01-17 21:23:51 +0600

    Create LeaderBoard.swift

---
+++ FizzBuzzPopReaction/FizzBuzzPopReaction/LeaderBoard.swift
@@ -0,0 +1,47 @@
+//
+//  LeaderBoard.swift
+//  FizzBuzzPopReaction
+//
+//  Created by Vasiliy Shannikov on 08.01.2022.
+//
+
+import SwiftUI
+
+struct LeaderBoard: View {
+    init(data1: ProgrammData) {
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+        UITableView.appearance().backgroundColor = .black
+        self.data = data1
+    }
+    @ObservedObject var data: ProgrammData
+    var gameScores = Storage.gameHistory.keys.sorted{ $0 > $1}
+    var body: some View {
+        VStack{
+           // Text("Leaderboard")
+            List{
+                ForEach(0..<data.scores.count) { i in
+                    HStack{
+                        Text("\(data.names[i])")
+                        Spacer()
+                        Text("\(data.scores[i])")
+                    }   .listRowBackground(Color.black)
+             
+                }
+            }
+            
+
+            Spacer()
+        }
+        .background(.black)
+        .foregroundColor(.white)
+        .navigationTitle("Leaderboard")
+        .accentColor(.white)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LeaderBoard_Previews: PreviewProvider {
+    static var previews: some View {
+        LeaderBoard(data1: ProgrammData())
+    }
+}
