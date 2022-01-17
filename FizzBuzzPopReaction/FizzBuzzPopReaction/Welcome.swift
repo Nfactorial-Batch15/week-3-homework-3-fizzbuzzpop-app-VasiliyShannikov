3468862d152783cbdef6e11788854999b52895b8
VasiliyShannikov <95113531+VasiliyShannikov@users.noreply.github.com>
2022-01-17 21:24:09 +0600

    Create Welcome.swift

---
+++ FizzBuzzPopReaction/FizzBuzzPopReaction/Welcome.swift
@@ -0,0 +1,69 @@
+//
+//  Welcome.swift
+//  FizzBuzzPopReaction
+//
+//  Created by Vasiliy Shannikov on 03.01.2022.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    @State var isNext = false
+    @State var isVisible = false
+    @ObservedObject var data: ProgrammData
+    @Binding var isGreating: Bool
+   // var arrrr = makeList(8)
+    var body: some View {
+      
+            ZStack {
+                Color.black
+                    .edgesIgnoringSafeArea(.all)
+                VStack(alignment: .center, spacing: 0) {
+//                    NavigationLink(isActive: $isNext) {
+//                        Main (data: data)
+//                    } label: {
+//                        EmptyView()
+//                    }
+
+                Text("ARE               YOU           READY?")
+                    .font(.system(size: 64, weight: .bold, design: .default))
+                    .foregroundColor(.white)
+                    .lineLimit(4)
+               Spacer()
+                
+                Button {
+                    isVisible = true
+                    isNext = true
+                    UserDefaults.standard.set(isNext, forKey: "isGreating")
+                  
+                  
+                }
+            label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .fill(.yellow)
+                    Text("Let's start")
+                        .font(.system(size: 16, weight: .medium, design: .default))
+                        .foregroundColor(.black)
+                }
+                .frame( height: 50, alignment: .center)
+                .padding(.horizontal,16)
+            }
+            
+            }
+                .background(.black)
+                
+                if isVisible{
+                    Main(data1: data)
+                }
+            }
+           
+
+        }
+}
+
+struct Welcome_Previews: PreviewProvider {
+    static var previews: some View {
+        Welcome(data: ProgrammData(), isGreating: .constant(true))
+    }
+}
